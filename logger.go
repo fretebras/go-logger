6bc9fdb48e90874a9bae84e01e36779e2e40f5ed
simoesfelippe <felippesteixeira@gmail.com>
2023-04-03 16:44:42 -0300

    chore: add methdo logger

--- logger.go
+++ logger.go
@@ -35,32 +35,32 @@ func init() {
 		Logger()
 }
 
-func Debug(Message string, Context ...string) {
+func (l *Logger) Debug(Message string, Context ...string) {
 	l.logger.Debug().Str("service", l.service).Strs("context", Context).Msg(Message)
 }
 
-func Info(Message string, Context ...string) {
+func (l *Logger) Info(Message string, Context ...string) {
 	l.logger.Info().
 		Str("service", l.service).Strs("context", Context).Msg(Message)
 }
 
-func Warning(Message string, Context ...string) {
+func (l *Logger) Warning(Message string, Context ...string) {
 	l.logger.Warn().
 		Str("service", l.service).Strs("context", Context).Msg(Message)
 }
 
-func Error(Error error, Context ...string) {
+func (l *Logger) Error(Error error, Context ...string) {
 	l.logger.Error().Str("service", l.service).Strs(
 		"context", Context).
 		Caller(1).Msg(Error.Error())
 }
 
-func Critical(Error error, Context ...string) {
+func (l *Logger) Critical(Error error, Context ...string) {
 	l.logger.Fatal().Str("service", l.service).Strs(
 		"context", Context).
 		Caller(1).Msg(Error.Error())
 }
 
-func SetLogService(service string) {
+func (l *Logger) SetLogService(service string) {
 	l.service = service
 }
