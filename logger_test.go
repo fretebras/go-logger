6668d1f07ca02f99a2878cfe8ced65c3a94767ed
simoesfelippe <felippesteixeira@gmail.com>
2023-04-25 19:50:51 -0300

    chore: add unit tests v0.1.4

--- logger_test.go
+++ logger_test.go
@@ -1,1 +1,65 @@
-package logger_test
+package logger
+
+import (
+	"bytes"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLogger_Debug(t *testing.T) {
+	logger := New()
+	logger.SetLogService("test-service")
+	var buf bytes.Buffer
+	logger.logger = logger.logger.Output(&buf)
+	logger.Debug("debug message", "context1")
+	assert.Contains(t, buf.String(), "debug message")
+	assert.Contains(t, buf.String(), "test-service")
+	assert.Contains(t, buf.String(), "context1")
+}
+
+func TestLogger_Info(t *testing.T) {
+	logger := New()
+	logger.SetLogService("test-service-info")
+	var buf bytes.Buffer
+	logger.logger = logger.logger.Output(&buf)
+	logger.Info("info message", "context-info")
+	assert.Contains(t, buf.String(), "info message")
+	assert.Contains(t, buf.String(), "test-service-info")
+	assert.Contains(t, buf.String(), "context-info")
+}
+
+func TestLogger_Warning(t *testing.T) {
+	logger := New()
+	logger.SetLogService("test-service-warning")
+	var buf bytes.Buffer
+	logger.logger = logger.logger.Output(&buf)
+	logger.Info("warning message", "context-warning")
+	assert.Contains(t, buf.String(), "warning message")
+	assert.Contains(t, buf.String(), "test-service-warning")
+	assert.Contains(t, buf.String(), "context-warning")
+}
+
+func TestLogger_Error(t *testing.T) {
+	logger := New()
+	logger.SetLogService("test-service-error")
+	var buf bytes.Buffer
+	logger.logger = logger.logger.Output(&buf)
+	err := errors.New("test error log")
+	logger.Error(err, "context-error")
+	assert.Contains(t, buf.String(), err.Error())
+	assert.Contains(t, buf.String(), "test-service-error")
+	assert.Contains(t, buf.String(), "context-error")
+}
+
+func TestLogger_Critical(t *testing.T) {
+	logger := New()
+	logger.SetLogService("test-service-critical")
+	var buf bytes.Buffer
+	logger.logger = logger.logger.Output(&buf)
+	err := errors.New("test critical log")
+	logger.Error(err, "context-critical")
+	assert.Contains(t, buf.String(), err.Error())
+	assert.Contains(t, buf.String(), "test-service-critical")
+	assert.Contains(t, buf.String(), "context-critical")
+}
--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,1 +1,34 @@
 package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestNewConfig(t *testing.T) {
+	// Cria um arquivo .config.env com valores para as variáveis de ambiente
+	err := ioutil.WriteFile(".config.env", []byte("APP=myapp\nENVIRONMENT=myenv\nVERSION=1.0"), 0644)
+	assert.NoError(t, err)
+
+	// Defina as variáveis de ambiente temporariamente
+	os.Setenv("APP", "myapp")
+	os.Setenv("ENVIRONMENT", "myenv")
+	os.Setenv("VERSION", "1.0")
+
+	// Chame a função NewConfig() e verifique os valores
+	config := NewConfig()
+	assert.Equal(t, "myapp", config.App)
+	assert.Equal(t, "myenv", config.Environment)
+	assert.Equal(t, "1.0", config.Version)
+
+	// Restaure as variáveis de ambiente para os valores originais
+	os.Unsetenv("APP")
+	os.Unsetenv("ENVIRONMENT")
+	os.Unsetenv("VERSION")
+
+	// Remove o arquivo .config.env criado
+	err = os.Remove(".config.env")
+	assert.NoError(t, err)
+}
